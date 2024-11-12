c34fac336afdca6818f8efcdcb514b9788cae0af
Mustafa Ahmed Raza <58116456+MustafaAR1@users.noreply.github.com>
2024-11-12 14:55:39 +0500

    registeration validation done

--- controller/account/register.js
+++ controller/account/register.js
@@ -1,16 +1,27 @@
 import stringConstants from "../../constants/string_constants.js";
 import user from "../../models/user.js";
 import bcrypt from "bcrypt";
+import Joi from "joi";
 
 const register = async (req, res, next) => {
-    const { username, email, password } = req.body;
+    const { email, username, password } = req.body;
+    const { error: validationError } = userValidation(req.body);
 
 
     try {
+        if (validationError) {
+            return res.status(400).json({ status: false, message: validationError.details[0].message });
+        }
+
+
         const formatteredUsername = username.toLowerCase();
         const formattedEmail = email.toLowerCase();
-        const existingUser = await user.findOne({ email });
-        if (existingUser) {
+        const existingUser = await user.findOne({ $or: [
+            { email: formattedEmail },
+            { username: formatteredUsername }
+        ] });
+
+        if (existingUser ) {
             return res.status(400).json({ status: false, message: stringConstants.USER_ALREADY_EXISTS });
 
         }
@@ -29,4 +40,19 @@ const register = async (req, res, next) => {
         next(error);
     }
 }
+
+function userValidation(data) {
+
+    const userSchema = Joi.object({
+        username: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).max(8).required(),
+    });
+    return userSchema.validate(data);
+
+
+}
+
+
+
 export default register;
